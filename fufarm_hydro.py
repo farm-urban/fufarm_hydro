7a828089adcd2d8ed8cbf392d34204c7d59d2534
Jens Thomas <linucks42@gmail.com>
2024-08-05 20:18:36 +0100

    update

--- fufarm_hydro.py
+++ fufarm_hydro.py
@@ -54,3 +54,26 @@ def control_ec():
         if time.time() - last_dose_time > dose_interval:
             last_dose_time = time.time()
             ec_pump.run(dose_interval)
+    return last_dose_time
+
+def toggle_control():
+    """Check for message to toggle control"""
+    return
+
+def should_run_clibration():
+    """Check for message to calibrate"""
+    return
+
+
+control = True
+while True:
+    if should_run_clibration():
+        ec_sensor.calibrate()
+
+    if toggle_control():
+        control = not control
+
+    if control:
+        last_dose_time = control_ec(last_dose_time)
+
+    time.sleep(1)
