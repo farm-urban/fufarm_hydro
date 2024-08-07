8c99b69df12951b4dd27500569b433380de17c0c
Jens Thomas <linucks42@gmail.com>
2024-08-07 18:56:48 +0100

    Ongoing updates

--- fufarm_hydro.py
+++ fufarm_hydro.py
@@ -22,7 +22,17 @@ last_dose_time
 Check aiomqtt for async MQTT client
 """
 
+import logging
+import json
 import time
+import paho.mqtt.client as mqtt
+
+logging.basicConfig(
+    level="INFO",
+    format=f"%(asctime)s rpi: %(message)s",
+)
+logger = logging.getLogger()
+
 
 dose_interval = 60.0
 
@@ -33,19 +43,7 @@ last_dose_time = time.time()
 ec_pump = Pump(1)
 
 
-def on_message(client, userdata, message):
-    topic = message.topic
-    payload = message.payload.decode("utf-8")
-    if topic == "farm/calibrate":
-        if payload == "ec":
-            ec_sensor.calibrate()
-    elif topic == "farm/parameters":
-        dose_duration, dose_interval = payload.split(",")
-    elif topic == "farm/control":
-        if payload == "monitor":
-            ec_pump.stop()
-
-def read_ec():
+# def read_ec():
 
 
 def control_ec():
@@ -56,17 +54,61 @@ def control_ec():
             ec_pump.run(dose_interval)
     return last_dose_time
 
+
 def toggle_control():
     """Check for message to toggle control"""
     return
+
 
 def should_run_clibration():
     """Check for message to calibrate"""
     return
 
 
+def on_message(client, userdata, message):
+    topic = message.topic
+    payload = message.payload.decode("utf-8")
+    if topic == "farm/calibrate":
+        if payload == "ec":
+            ec_sensor.calibrate()
+    elif topic == "farm/parameters":
+        try:
+            data = json.loads(payload)
+        except json.decoder.JSONDecodeError as e:
+            logger.warning(
+                "Error decoding MQTT data to JSON: %s\nMessage was: %s", e.msg, e.doc
+            )
+        dose_duration = data.get("dose_duration", 1)
+        dose_interval = data.get("dose_interval", 60)
+    elif topic == "farm/control":
+        if payload == 0:
+            ec_pump.stop()
+
+
+def setup_mqtt():
+    client = mqtt.Client()
+    client.username_pw_set(CONFIG.MQTT.username, CONFIG.MQTT.password)
+    ret = client.connect(CONFIG.MQTT.host, port=CONFIG.MQTT.port)
+    logger.debug(f"MQTT client connect return code: {ret}")
+
+    # Add different plugs
+    for topic in ["farm/calibrate", "farm/parameters", "farm/control"]:
+        client.subscribe(topic)
+    client.on_message = on_message
+
+    return client
+
+
+client = setup_mqtt()
+client.loop_start()
+
 control = True
 while True:
+    # Below seems to raise an exception - not sure why
+    if not client.is_connected():
+        logger.error("mqtt_client not connected")
+        client.reconnect()
+
     if should_run_clibration():
         ec_sensor.calibrate()
 
