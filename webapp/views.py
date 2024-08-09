3f39e5014fe5f75b173a1346422df05ab1de1e70
Jens Thomas <linucks42@gmail.com>
2024-08-09 17:17:39 +0100

    Latest updated

--- webapp/views.py
+++ webapp/views.py
@@ -2,6 +2,8 @@ from flask import jsonify
 from flask import render_template
 from flask import request
 
+import random
+import time
 from . import app
 
 
@@ -15,3 +17,14 @@ def add():
     a = request.form.get("a", 0, type=float)
     b = request.form.get("b", 0, type=float)
     return jsonify(result=a + b)
+
+
+@app.route("/status")
+def status():
+    q = request.args.get("q")
+    if q == "current_ec":
+        return jsonify(ec=round(random.uniform(0.0, 3.0), 2))
+    elif q == "last_dose_time":
+        return jsonify(
+            last_dose_time=time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime())
+        )
