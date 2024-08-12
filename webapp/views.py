057d39cc67842a12f0fb8fa7cc01d4fc0dead798
Jens Thomas <linucks42@gmail.com>
2024-08-12 22:14:29 +0100

    Working to set control

--- webapp/views.py
+++ webapp/views.py
@@ -1,7 +1,10 @@
 from flask import jsonify
 from flask import render_template
+from flask import request
 
-from . import app, app_state
+from . import app, app_state, mqtt, mqtt_topics
+
+from util import ID_CONTROL
 
 
 @app.route("/")
@@ -9,9 +12,32 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/control", methods=["POST"])
+def control():
+    mode = request.form["mode"]
+    changed = False
+    if mode == "control" and not app_state.control:
+        app_state.control = True
+        changed = True
+    elif mode == "monitor" and app_state.control:
+        app_state.control = False
+        changed = True
+    if changed:
+        print("Publishing control: ", app_state.control)
+        mqtt.publish(mqtt_topics[ID_CONTROL], "1" if app_state.control else "0")
+
+    # target_ec = request.form["target-ec"]
+    # if target_ec:
+    #     app_state.target_ec = float(target_ec)
+    #     print("Publishing target_ec: ", app_state.target_ec)
+    #     mqtt.publish(mqtt_topics[ID_CONTROL], str(app_state.target_ec))
+
+    data = {"status": "success"}
+    return data, 200
+
+
 @app.route("/status")
 def status():
-    # from flask import request
     # q = request.args.get("q")
     # if q == "current_ec":
     #     return jsonify(ec=round(app_state.current_ec, 2))
