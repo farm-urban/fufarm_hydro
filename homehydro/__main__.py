f39bb6f9117b1f1d5f0ea763c3e3cba333c09a1c
Jens Thomas <linucks42@gmail.com>
2024-08-22 12:02:22 +0100

    Handle flask reload loop

--- homehydro/__main__.py
+++ homehydro/__main__.py
@@ -1,6 +1,7 @@
 """"Main Flask App"""
 
 import logging
+import os
 import threading
 from argparse import ArgumentParser
 
@@ -14,10 +15,14 @@ parser.add_argument(
     "-c", "--config", required=True, dest="app_config", help="Path to the config file"
 )
 parser.add_argument(
-    "-d", "--debug", action='store_true', dest="debug", help="Turn on debug mode"
+    "-d", "--debug", action="store_true", dest="debug", help="Turn on debug mode"
 )
 parser.add_argument(
-    "-m", "--mqttio-config", required=True, dest="mqttio_config_file", help="Path to the MQTT-IO config file"
+    "-m",
+    "--mqttio-config",
+    required=True,
+    dest="mqttio_config_file",
+    help="Path to the MQTT-IO config file",
 )
 args = parser.parse_args()
 config_file = args.app_config
@@ -30,11 +35,20 @@ logging.basicConfig(
 )
 
 app.config["APP_STATE"] = app_state
-hydro_controller = controller.HydroController(app_config, app_state, mqttio_config_file)
-thread = threading.Thread(target=hydro_controller.run)
+
+# In debug mode flask forks intself so we don't want to start the controller twice
+# https://raspberrypi.stackexchange.com/questions/148825/lgpio-gpio-setup-fails-with-gpio-not-allocated-when-run-from-a-flask-app
+MAIN_LOOP = False
+if os.environ.get("WERKZEUG_RUN_MAIN") == "true":
+    MAIN_LOOP = True
+    hydro_controller = controller.HydroController(
+        app_config, app_state, mqttio_config_file
+    )
+    thread = threading.Thread(target=hydro_controller.run)
 
 from . import views  # noqa: E402, F401
 
 if __name__ == "__main__":
-    thread.start()
+    if MAIN_LOOP:
+        thread.start()
     app.run(host=app_config.flask_host, debug=args.debug)
