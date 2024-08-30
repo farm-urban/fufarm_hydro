2d85d7733584fa582ef2d090eb0097b43eedccc9
linucks <linucks42@gmail.com>
2024-08-30 10:19:48 +0100

    Make able to run from ansible

--- activate_venv.sh
+++ activate_venv.sh
@@ -1,9 +1,11 @@
+#/bin/bash
+VENV_DIR=venv
 if [ ! -d ./venv ]; then
   echo "Creating virtual python environment"
-  python3 -m venv venv 
-  . venv/bin/activate
+  python3 -m venv ${VENV_DIR}
+  . ${VENV_DIR}/bin/activate
   pip install .
 fi
 
 echo "Activating virtual python environment"
-. venv/bin/activate
+. ${VENV_DIR}/bin/activate
