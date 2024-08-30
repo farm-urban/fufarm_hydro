fb500b81c8683ccda0d35f98beadc96e2018444e
linucks <linucks42@gmail.com>
2024-08-30 15:18:02 +0100

    Fix flask reloader issue

--- homehydro/__main__.py
+++ homehydro/__main__.py
@@ -5,6 +5,8 @@ import os
 import threading
 from argparse import ArgumentParser
 
+from werkzeug.serving import is_running_from_reloader
+
 from homehydro.hydrocontrol.state_classes import process_config
 import homehydro.hydrocontrol.controller as controller
 
@@ -33,13 +35,14 @@ logging.basicConfig(
     level=app_config.log_level,
     format="%(asctime)s rpi: %(message)s",
 )
+_LOG = logging.getLogger(__name__)
 
 app.config["APP_STATE"] = app_state
 
 # In debug mode flask forks intself so we don't want to start the controller twice
 # https://raspberrypi.stackexchange.com/questions/148825/lgpio-gpio-setup-fails-with-gpio-not-allocated-when-run-from-a-flask-app
 MAIN_LOOP = False
-if os.environ.get("WERKZEUG_RUN_MAIN") == "true":
+if not is_running_from_reloader():
     MAIN_LOOP = True
     hydro_controller = controller.HydroController(
         app_config, app_state, mqttio_config_file
